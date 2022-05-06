880fd5f9701477ca8cff32d2f1241e6e13d7dd22
Tony1257 <34118938+1257@users.noreply.github.com>
2022-05-06 17:30:44 +0300

    Update transform_labels.py

--- transform_labels.py
+++ transform_labels.py
@@ -1,7 +1,7 @@
 def my_entropy(outputs, labels):
     coarse = []
-    real_superclass = []*128
-    
+    real_superclass = [None]*128
+    print(real_superclass)
 
     for i in range(128):
         coarse.append([])
@@ -50,25 +50,25 @@ def my_entropy(outputs, labels):
 
     for i in range(128):
         coarse[i].append(sum([outputs[i][72], outputs[i][4], outputs[i][95], outputs[i][30], outputs[i][55]]))
-        coarse[i].append(sum(outputs[i][73], outputs[i][32], outputs[i][67], outputs[i][91], outputs[i][1]))
-        coarse[i].append(sum(outputs[i][92], outputs[i][70], outputs[i][82], outputs[i][54], outputs[i][62]))
-        coarse[i].append(sum(outputs[i][16], outputs[i][61], outputs[i][9], outputs[i][10], outputs[i][28]))
-        coarse[i].append(sum(outputs[i][51], outputs[i][0], outputs[i][53], outputs[i][57], outputs[i][83]))
-        coarse[i].append(sum(outputs[i][40], outputs[i][39], outputs[i][22], outputs[i][87], outputs[i][86]))
-        coarse[i].append(sum(outputs[i][20], outputs[i][25], outputs[i][94], outputs[i][84], outputs[i][5]))
-        coarse[i].append(sum(outputs[i][14], outputs[i][24], outputs[i][6], outputs[i][7], outputs[i][18]))
-        coarse[i].append(sum(outputs[i][43], outputs[i][97], outputs[i][42], outputs[i][3], outputs[i][88]))
-        coarse[i].append(sum(outputs[i][37], outputs[i][17], outputs[i][76], outputs[i][12], outputs[i][68]))
-        coarse[i].append(sum(outputs[i][49], outputs[i][33], outputs[i][71], outputs[i][23], outputs[i][60]))
-        coarse[i].append(sum(outputs[i][15], outputs[i][21], outputs[i][19], outputs[i][31], outputs[i][38]))
-        coarse[i].append(sum(outputs[i][75], outputs[i][63], outputs[i][64], outputs[i][66], outputs[i][34]))
-        coarse[i].append(sum(outputs[i][77], outputs[i][26], outputs[i][45], outputs[i][99], outputs[i][79]))
-        coarse[i].append(sum(outputs[i][11], outputs[i][2], outputs[i][35], outputs[i][46], outputs[i][98]))
-        coarse[i].append(sum(outputs[i][29], outputs[i][93], outputs[i][27], outputs[i][78], outputs[i][44]))
-        coarse[i].append(sum(outputs[i][65], outputs[i][50], outputs[i][74], outputs[i][36], outputs[i][80]))
-        coarse[i].append(sum(outputs[i][56], outputs[i][52], outputs[i][47], outputs[i][59], outputs[i][96]))
-        coarse[i].append(sum(outputs[i][8], outputs[i][58], outputs[i][90], outputs[i][13], outputs[i][48]))
-        coarse[i].append(sum(outputs[i][81], outputs[i][69], outputs[i][41], outputs[i][89], outputs[i][85]))
+        coarse[i].append(sum([outputs[i][73], outputs[i][32], outputs[i][67], outputs[i][91], outputs[i][1]]))
+        coarse[i].append(sum([outputs[i][92], outputs[i][70], outputs[i][82], outputs[i][54], outputs[i][62]]))
+        coarse[i].append(sum([outputs[i][16], outputs[i][61], outputs[i][9], outputs[i][10], outputs[i][28]]))
+        coarse[i].append(sum([outputs[i][51], outputs[i][0], outputs[i][53], outputs[i][57], outputs[i][83]]))
+        coarse[i].append(sum([outputs[i][40], outputs[i][39], outputs[i][22], outputs[i][87], outputs[i][86]]))
+        coarse[i].append(sum([outputs[i][20], outputs[i][25], outputs[i][94], outputs[i][84], outputs[i][5]]))
+        coarse[i].append(sum([outputs[i][14], outputs[i][24], outputs[i][6], outputs[i][7], outputs[i][18]]))
+        coarse[i].append(sum([outputs[i][43], outputs[i][97], outputs[i][42], outputs[i][3], outputs[i][88]]))
+        coarse[i].append(sum([outputs[i][37], outputs[i][17], outputs[i][76], outputs[i][12], outputs[i][68]]))
+        coarse[i].append(sum([outputs[i][49], outputs[i][33], outputs[i][71], outputs[i][23], outputs[i][60]]))
+        coarse[i].append(sum([outputs[i][15], outputs[i][21], outputs[i][19], outputs[i][31], outputs[i][38]]))
+        coarse[i].append(sum([outputs[i][75], outputs[i][63], outputs[i][64], outputs[i][66], outputs[i][34]]))
+        coarse[i].append(sum([outputs[i][77], outputs[i][26], outputs[i][45], outputs[i][99], outputs[i][79]]))
+        coarse[i].append(sum([outputs[i][11], outputs[i][2], outputs[i][35], outputs[i][46], outputs[i][98]]))
+        coarse[i].append(sum([outputs[i][29], outputs[i][93], outputs[i][27], outputs[i][78], outputs[i][44]]))
+        coarse[i].append(sum([outputs[i][65], outputs[i][50], outputs[i][74], outputs[i][36], outputs[i][80]]))
+        coarse[i].append(sum([outputs[i][56], outputs[i][52], outputs[i][47], outputs[i][59], outputs[i][96]]))
+        coarse[i].append(sum([outputs[i][8], outputs[i][58], outputs[i][90], outputs[i][13], outputs[i][48]]))
+        coarse[i].append(sum([outputs[i][81], outputs[i][69], outputs[i][41], outputs[i][89], outputs[i][85]]))
         
     print('outputs: ', outputs[0])
     print('outputs of superclasses', coarse[0])
