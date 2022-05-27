2cfee07912d076800e7fa60f20ce5d8b53d227f8
Tony1257 <34118938+1257@users.noreply.github.com>
2022-05-27 09:56:36 +0300

    Update entropy_2_levels.py

--- entropy_2_levels.py
+++ entropy_2_levels.py
@@ -106,8 +106,8 @@ def modifiedEntropy2lvl(outputs, labels):
         #print()
     
     l1=loss(outputs, labels)
-    l2=loss(torch.cat(coarse, 0), real_superclass)
-    #l2=loss(coarse, real_superclass)
+    #l2=loss(torch.cat(coarse, 0), real_superclass)
+    l2=loss(coarse, real_superclass)
     #print("class loss =", l1, "; superclass loss =", l2)
     
     c=input()
