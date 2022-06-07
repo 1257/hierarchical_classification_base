fa15bfe5eb3dd5975e97ef6f6037d920d8c85036
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-07 15:06:39 +0300

    Update entropy_2_levels.py

--- entropy_2_levels.py
+++ entropy_2_levels.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch
 
-def entropy2lvl(outputs, labels):
+def entropy2lvl(outputs, labels, class_labels):
     loss = nn.CrossEntropyLoss()
     coarse = []
     real_superclass = [None]*len(outputs)
@@ -10,48 +10,6 @@ def entropy2lvl(outputs, labels):
 
     for i in range(len(outputs)):
         coarse.append([])
-        ind=labels[i]
-        if (ind in [72, 4, 95, 30, 55]):
-          real_superclass[i]=0
-        elif ind in [73, 32, 67, 91, 1]:
-          real_superclass[i]=1
-        elif ind in [92, 70, 82, 54, 62]:
-          real_superclass[i]=2
-        elif ind in [16, 61, 9, 10, 28]:
-          real_superclass[i]=3
-        elif ind in [51, 0, 53, 57, 83]:
-          real_superclass[i]=4
-        elif ind in [40, 39, 22, 87, 86]:
-          real_superclass[i]=5
-        elif ind in [20, 25, 94, 84, 5]:
-          real_superclass[i]=6
-        elif ind in [14, 24, 6, 7, 18]:
-          real_superclass[i]=7
-        elif ind in [43, 97, 42, 3, 88]:
-          real_superclass[i]=8
-        elif ind in [37, 17, 76, 12, 68]:
-          real_superclass[i]=9
-        elif ind in [49, 33, 71, 23, 60]:
-          real_superclass[i]=10
-        elif ind in [15, 21, 19, 31, 38]:
-          real_superclass[i]=11
-        elif ind in [75, 63, 64, 66, 34]:
-          real_superclass[i]=12
-        elif ind in [77, 26, 45, 99, 79]:
-          real_superclass[i]=13
-        elif ind in [11, 2, 35, 46, 98]:
-          real_superclass[i]=14
-        elif ind in [29, 93, 27, 78, 44]:
-          real_superclass[i]=15
-        elif ind in [65, 50, 74, 36, 80]:
-          real_superclass[i]=16
-        elif ind in [56, 52, 59, 47, 96]:
-          real_superclass[i]=17
-        elif ind in [8, 58, 90, 13, 48]:
-          real_superclass[i]=18
-        elif ind in [81, 69, 41, 89, 85]:
-          real_superclass[i]=19
-    #print(real_superclass)
 
     for i in range(len(outputs)):
         coarse[i].append(func([outputs[i][72], outputs[i][4], outputs[i][95], outputs[i][30], outputs[i][55]]))
@@ -74,14 +32,20 @@ def entropy2lvl(outputs, labels):
         coarse[i].append(func([outputs[i][56], outputs[i][52], outputs[i][47], outputs[i][59], outputs[i][96]]))
         coarse[i].append(func([outputs[i][8], outputs[i][58], outputs[i][90], outputs[i][13], outputs[i][48]]))
         coarse[i].append(func([outputs[i][81], outputs[i][69], outputs[i][41], outputs[i][89], outputs[i][85]]))
+            
         
-    #print('outputs: ', outputs[0])
-    #print('outputs of superclasses', coarse[0])
-    #print('real superclass:', real_superclass)
     l1=loss(outputs, labels)
-    l2=loss(torch.tensor(coarse), torch.tensor(real_superclass))
+    #l2=loss(torch.tensor(coarse), torch.tensor(real_superclass))
+    
+    for i in range(len(outputs)): 
+        if class_labels[i]==-1:
+            del class_labels[i]
+            del coarse[i]
+            
+    l2=loss(torch.tensor(coarse), torch.tensor(class_labels))
+    
     #print("class loss =", l1, "; superclass loss =", l2)
-    return 0.3*l1+0.7*l2
+    return l1+l2
 
 
 def modifiedEntropy2lvl(outputs, labels):
