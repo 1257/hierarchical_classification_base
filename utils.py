fc212c3e381f2f4a8beb89ef6318868c5b81ed9f
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-07 12:22:11 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -329,7 +329,7 @@ def get_training_dataloader_with_hierarhy(is_new_set, mean, std, batch_size=16, 
     
     
     for i in range(len(cifar100_trainset1)):
-      cifar100_trainset1[i][2]=csuperclass[cifar100_trainset1[i][1]]
+      cifar100_trainset1[i][2]=superclass[cifar100_trainset1[i][1]]
       if i<21:
         print(cifar100_trainset1[i])
         
