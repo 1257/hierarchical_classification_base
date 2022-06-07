4936e9b1fa5c1e86f99129a5e5b2923d117558c1
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-07 12:42:22 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -335,7 +335,7 @@ def get_training_dataloader_with_hierarhy(is_new_set, mean, std, batch_size=16, 
       cifar100_trainset1[i].append(superclass[cifar100_trainset1[i][1]])
       cifar100_trainset1[i]=tuple(cifar100_trainset1[i])
       if i<21:
-        print(cifar100_trainset1[i][1:2])
+        print(cifar100_trainset1[i][1:3])
         
     #cifar100_trainset1=torch.tensor(cifar100_trainset1)
         
