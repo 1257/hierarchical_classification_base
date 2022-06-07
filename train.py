284ec6deca5c600ff4f4263d4b61461c9d85c9c9
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-07 16:43:28 +0300

    Update train.py

--- train.py
+++ train.py
@@ -132,7 +132,7 @@ def eval_training(loss_function, cifar100_test_loader, epoch=0, tb=True, ):
         preds_super = torch.tensor(preds_super)
         class_labels = [class_labels[i] for i in range(len(class_labels)) if class_labels[i]!=-1]
         
-        correct1 += preds.eq(class_labels).sum()
+        correct1 += preds.eq(torch.tensor(class_labels)).sum()
         correct2 += preds_super.eq(torch.tensor(labels)).sum()
 
         print("real classes:", class_labels)
