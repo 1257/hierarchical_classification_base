1e85cc4aebf111474851aa6b57c879cf3ee69754
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-07 15:33:46 +0300

    Update entropy_2_levels.py

--- entropy_2_levels.py
+++ entropy_2_levels.py
@@ -43,14 +43,14 @@ def entropy2lvl(outputs, labels, class_labels):
     #        class_labels[i] = torch.cat([class_labels[0:i], class_labels[i+1:]], axis=0)
     #        coarse[i] = torch.cat([coarse[0:i], coarse[i+1:]], axis=0)
     
-    print("all classes:", class_labels)
+    #print("all classes:", class_labels)
     
     coarse=list(coarse)
     coarse=[coarse[i] for i in range(len(class_labels)) if class_labels[i]!=-1]
     class_labels=list(class_labels)
     class_labels=[class_labels[i] for i in range(len(class_labels)) if class_labels[i]!=-1]
     
-    print("existing classes:", class_labels)
+    #print("existing classes:", class_labels)
     
     l2=loss(torch.tensor(coarse), torch.tensor(class_labels))
     
