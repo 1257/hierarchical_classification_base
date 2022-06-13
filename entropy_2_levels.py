8ae7d15fe7757ad3f295e669eed986bb1ef2d634
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-13 23:47:59 +0300

    Update entropy_2_levels.py

--- entropy_2_levels.py
+++ entropy_2_levels.py
@@ -42,7 +42,7 @@ def entropy2lvl(outputs, labels, class_labels, use_superclasses=True, use_classe
     indices = torch.nonzero(mask)
     print("all: ", outputs)
     outs = outputs[indices]
-    outs = outs[0]
+    #outs = outs[0]
     outs = (torch.tensor(outs)).cuda()
     print("filtered: ", outputs[indices])
     print("filtered[0]: ", outs)
