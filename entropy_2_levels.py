de2e095e3d39ec5af7908508ac9d66e4d817e4c9
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-13 20:31:55 +0300

    Update entropy_2_levels.py

--- entropy_2_levels.py
+++ entropy_2_levels.py
@@ -37,7 +37,8 @@ def entropy2lvl(outputs, labels, class_labels, use_superclasses=True, use_classe
     l1=loss(torch.tensor(coarse).cuda(), labels)    
     
     class_labels1=list(class_labels)
-    outputs1=outputs.cuda()
+    outputs1=list(outputs)
+    #outputs1=outputs.cuda()
     
     outputs1=[outputs[i] for i in range(len(class_labels)) if class_labels1[i]!=-1]
     class_labels1=[class_labels1[i] for i in range(len(class_labels1)) if class_labels1[i]!=-1]
