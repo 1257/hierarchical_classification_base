4f9dd8fca81e9a32551bc0b2cab31f9cf712e78e
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-13 23:56:52 +0300

    Update entropy_2_levels.py

--- entropy_2_levels.py
+++ entropy_2_levels.py
@@ -46,6 +46,7 @@ def entropy2lvl(outputs, labels, class_labels, use_superclasses=True, use_classe
     outs = (torch.tensor(outs)).cuda()
     print("filtered: ", outputs[indices])
     print("filtered[0]: ", outs)
+    print("indexed labels: ", class_labels[indices])
     l2=loss(outs, class_labels[indices])   #loss on classes
     
     if use_superclasses and use_classes:
