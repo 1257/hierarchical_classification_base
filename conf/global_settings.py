693ff321e0368c6368f1df4c6a3f28da180ed351
Tony1257 <34118938+1257@users.noreply.github.com>
2022-06-13 21:39:48 +0300

    Update global_settings.py

--- conf/global_settings.py
+++ conf/global_settings.py
@@ -8,6 +8,8 @@ from datetime import datetime
 #CIFAR100 dataset path (python version)
 #CIFAR100_PATH = '/nfs/private/cifar100/cifar-100-python'
 
+CUDA_LAUNCH_BLOCKING=1
+
 #mean and std of cifar100 dataset
 CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
 CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
